76c36fe2b4f660785fe85d4282cc610da2998924
Akshay Kulkarni <Akshay.Kulkarni@clinisys.com>
2024-02-18 10:16:10 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,52 +1,27 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
-const cors = require('cors');
-app.listen(process.env.PORT || 3000)
-let records = [];
-
-app.use(cors());
-
-//Get all students
-router.get('/', (req, res) => {
-  res.send('App is running..');
-});
-
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
-
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
 //showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
-
-app.use('/.netlify/functions/api', router);
-
+app.get('/demo', (req, res) => {
+    res.json([
+      {
+        id: '001',
+        name: 'Smith',
+        email: 'smith@gmail.com',
+      },
+      {
+        id: '002',
+        name: 'Sam',
+        email: 'sam@gmail.com',
+      },
+      {
+        id: '003',
+        name: 'lily',
+        email: 'lily@gmail.com',
+      },
+    ]);
+  });
+app.listen(process.env.PORT || 3000)
